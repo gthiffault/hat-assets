dc916aa555b17c1eb0dbcf746b9cb112a68adaac
gthiffault <gabriel.thiffault@gmail.com>
2019-04-25 22:20:19 -0400

    Header and single

--- assets/script/vendor/f2stickyimages.js
+++ assets/script/vendor/f2stickyimages.js
@@ -2,3 +2,16 @@ $("#c-testimonials_image").stick_in_parent({
 	offset_top:124,
 	sticky_class:"c-element-fixed"
 });
+
+      $(function() {
+        // Initializes and creates emoji set from sprite sheet
+        window.emojiPicker = new EmojiPicker({
+          emojiable_selector: '[data-emojiable=true]',
+          assetsPath: 'http://hat.test/assets/img/emoji/',
+          popupButtonClasses: 'fa fa-smile-o'
+        });
+        // Finds all elements with `emojiable_selector` and converts them to rich emoji input fields
+        // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
+        // It can be called as many times as necessary; previously converted input fields will not be converted again
+        window.emojiPicker.discover();
+      });
