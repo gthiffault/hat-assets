1fe8a0561b870e290b48ab7e4048f3c1ff5aceb4
gthiffault <gabriel.thiffault@gmail.com>
2019-08-09 17:59:22 -0400

    2

--- assets/script/app.js
+++ assets/script/app.js
@@ -143,28 +143,25 @@ $(document).ready(function() {
 
 
     (function _(a,b,c,d,e){var f=window.console;if(window[c])return;if(!window.JSON)return;var g=window[c]={__buffer:{replay:function(){var a=this,b=function(d){var b=window[c];a.calls[d][0].split(".").forEach(function(a){return b=b[a]});b.apply(null,a.calls[d][1])};for(var d=0;d<this.calls.length;d++)b(d);this.calls=[]},calls:[],opts:null},getUserID:function(){return""},getAuthResponse:function(){return null},getAccessToken:function(){return null},init:function(a){g.__buffer.opts=a}};for(var b=0;b<d.length;b++){f=d[b];if(f in g)continue;var h=f.split("."),i=h.pop(),j=g;for(var k=0;k<h.length;k++)j=j[h[k]]||(j[h[k]]={});j[i]=function(a){if(a==="init")return;return function(){g.__buffer.calls.push([a,Array.prototype.slice.call(arguments)])}}(f)}k=a;h=/Chrome\/(\d+)/.exec(navigator.userAgent);h&&Number(h[1])>=55&&"assign"in Object&&"findIndex"in[]&&(k+="&ua=modern_es6");j=document.createElement("script");j.src=k;j.async=!0;e&&(j.crossOrigin="anonymous");i=document.getElementsByTagName("script")[0];i.parentNode&&i.parentNode.insertBefore(j,i)})("https:\/\/connect.facebook.net\/fr_CA\/sdk.js?hash=eb854bb36b11e5d0f15ca21c2026c015", 1554433319, "FB", ["AppEvents.EventNames","AppEvents.ParameterNames","AppEvents.activateApp","AppEvents.clearAppVersion","AppEvents.clearUserID","AppEvents.getAppVersion","AppEvents.getUserID","AppEvents.logEvent","AppEvents.logPageView","AppEvents.logPurchase","AppEvents.setAppVersion","AppEvents.setUserID","AppEvents.updateUserProperties","Canvas.Plugin.showPluginElement","Canvas.Plugin.hidePluginElement","Canvas.Prefetcher.addStaticResource","Canvas.Prefetcher.setCollectionMode","Canvas.getPageInfo","Canvas.scrollTo","Canvas.setAutoGrow","Canvas.setDoneLoading","Canvas.setSize","Canvas.setUrlHandler","Canvas.startTimer","Canvas.stopTimer","Event.subscribe","Event.unsubscribe","XFBML.parse","addFriend","api","getAccessToken","getAuthResponse","getLoginStatus","getUserID","init","login","logout","publish","share","ui"], true);
-
-//intialize FB object (this is useful if you are using Turbolinks)
-$(document).on('page:load', function(){
- intializeFB();
-});
-
-intializeFB();
-  var array = [];
+    var array = [];
     var postID = '/432264780512618_' + $('.c-testimonials_id').text();
-function intializeFB(){
- if(typeof(FB) !== "undefined"){
-  delete FB;
- }
 
-  
-    window.fbAsyncInit = function() {
+$.ajax(
+            {
+                url: '//connect.facebook.net/en_US/sdk.js',
+                dataType: 'script',
+                cache: true,
+                success:function(script, textStatus, jqXHR)
+                {
+
         FB.init({
             appId            : '516971002166010',
             autoLogAppEvents : true,
             xfbml            : true,
             version          : 'v3.2'
         });
+     }
+});        
         FB.api(
             postID,
             'GET',
@@ -181,8 +178,7 @@ function intializeFB(){
                 }
             }
         );
-    };
-  }
+
     setTimeout(function() {
         var x;
         if(array.length > 4) {
