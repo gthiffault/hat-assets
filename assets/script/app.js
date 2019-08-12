379880587e536a7fcb8759590dd398b144ea15f8
gthiffault <gabriel.thiffault@gmail.com>
2019-08-12 19:48:30 -0400

    Fuck

--- assets/script/app.js
+++ assets/script/app.js
@@ -793,10 +793,27 @@ $('.c-header_navigation_mobile ul li a').on('click', function() {
   var hamburger = document.querySelector(".hamburger");
   // On click
   hamburger.addEventListener("click", function() {
+
+
+
+if($('.c-emergency_mobile_ul').is(':visible') {
+
+$(this).find('.c-emergency_mobile_ul').slideToggle();
+setTimeout(function() {
     // Toggle class "is-active"
     hamburger.classList.toggle("is-active");
     // Do something else, like open/close menu
     $('html').toggleClass('-js-menu-active');
+},500);
+} else {
+     // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+    $('html').toggleClass('-js-menu-active');
+ 
+}
+
+
   });
 
 $(window).resize(function() {
